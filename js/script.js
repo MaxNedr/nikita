41bcfffc5a7269eb4ea9cdf94d73294c6fa8cef1
NedrNout <maxim.nedozrelov@ya.ru>
2019-01-10 08:29:23 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+'use strict';
+/*document.querySelector('.button').onmousemove = (e) => {
+    const x = e.pageX - e.target.offsetLeft;
+    const y = e.pageY - e.target.offsetTop;
+    e.target.style.setProperty(' - x',
+        `${ x }px`
+    );
+    e.target.style.setProperty('- y',
+    `${ y }px`
+)
+};*/
+/*document.querySelector('.button').onmousemove = function (e) {
+
+    var x = e.pageX - e.target.offsetLeft;
+    var y = e.pageY - e.target.offsetTop;
+
+    e.target.style.setProperty('--x', x + 'px');
+    e.target.style.setProperty('--y', y + 'px');
+
+};*/
 $(window).scroll(function(){
     let el = $('#first_tape');
     if ( $(this).scrollTop() > el.offset().top - 1 ) {
@@ -7,4 +27,6 @@ $(window).scroll(function(){
     if ( $(this).scrollTop() > el2.offset().top + 3250 ) {
         el2.addClass('d-flex animated fadeIn ');
     }
+
 });
+
